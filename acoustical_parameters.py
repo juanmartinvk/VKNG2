b5410c31d44f4acb2917eb4366a0684f3f731718
Juan Martín van Keulen <juanmartinvk@gmail.com>
2021-04-21 10:39:30 -0300

    Excepciones Lundeby

--- acoustical_parameters.py
+++ acoustical_parameters.py
@@ -5,7 +5,7 @@ import soundfile as sf
 from scipy.signal import butter, sosfilt, medfilt
 from scipy.stats import linregress
 from statistics import mean
-import matplotlib.pyplot as plt
+
 
 
 
@@ -20,11 +20,16 @@ def parameters(file_path, b=1, truncate=None, smoothing='schroeder'):
     
 
     
-    #Define band index array
+    #Define band index array and nominal bands
     if b == 3:
         band_idx = np.arange(-16, 14)
+
+        nominal_bands = [25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250,
+                         315, 400, 500, 630, 800, 1000, 1300, 1600, 2000, 2500,
+                         3200, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000]
     elif b == 1:
         band_idx = np.arange(-5, 5)
+        nominal_bands = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
     else:
         print('invalid b')
     
@@ -32,7 +37,8 @@ def parameters(file_path, b=1, truncate=None, smoothing='schroeder'):
 
     maf_windows = np.linspace(0.01, 0.05, num=len(band_idx)) #Generate times between 10 and 50ms
     maf_windows = np.intc(maf_windows * fs)[::-1] #Convert to integer number of samples and invert
-
+    
+    #Initialize parameter lists
     ETC = []
     decay = []
     EDT = []
@@ -44,7 +50,7 @@ def parameters(file_path, b=1, truncate=None, smoothing='schroeder'):
     Tt = []
     EDTt = []
 
-    maf_window_idx = 0
+    counter = 0
     for idx in band_idx:
         #Band limits
         f_low, f_high = limits_iec_61260(idx, b)
@@ -60,7 +66,7 @@ def parameters(file_path, b=1, truncate=None, smoothing='schroeder'):
         
         #Truncate
         if truncate == 'lundeby':
-            ETC_truncated, crossing_point = lundeby(ETC_band, maf_windows[maf_window_idx], fs)
+            ETC_truncated, crossing_point = lundeby(ETC_band, maf_windows[counter], nominal_bands[counter], fs)
         elif truncate is None:
             ETC_truncated = ETC_band
             crossing_point = len(ETC_band)
@@ -78,22 +84,19 @@ def parameters(file_path, b=1, truncate=None, smoothing='schroeder'):
         
         
         #Append parameters to lists
-        #A los parámetros que no requieran la integración,  se les puede
-        #pasar directamente ETC_band o IR_filtered. Hay que ver qué onda
-        #para IACCe
+
         ETC.append(ETC_band)     
         decay.append(decay_band)
         EDT.append(EDT_from_IR(decay_band, fs))
         T20.append(T20_from_IR(decay_band, fs))
         T30.append(T30_from_IR(decay_band, fs))
-        C50.append(C50_from_IR(fs, ETC_band))
-        C80.append(C80_from_IR(fs, ETC_band))
+        C50.append(C50_from_IR(fs, ETC_truncated))
+        C80.append(C80_from_IR(fs, ETC_truncated))
        
        
-       
-        #etc...
+      
         
-        maf_window_idx += 1
+        counter += 1
         
     return ETC, decay, EDT, T20, T30, C50, C80
     #return  IACCe, Tt, EDTt
@@ -187,18 +190,20 @@ def T30_from_IR(signal, fs):
 
 
 def C50_from_IR(fs, ETC):
-    
+    #ETC = ETC[np.argmax(ETC):]
+    sch = np.cumsum(ETC[::-1])[::-1]
     t = int(0.05 * fs + 1) # 50ms samples
-    C50= 10.0 * np.log10((np.sum(ETC[:t]) / np.sum(ETC[t:])))
+    C50= 10.0 * np.log10((np.sum(sch[:t]) / np.sum(sch[t:])))
     
     return C50 
 
 
 
 def C80_from_IR(fs, ETC):
-    
+    #ETC = ETC[np.argmax(ETC):]
+    sch = np.cumsum(ETC[::-1])[::-1]
     t = int(0.08 * fs + 1) # 80ms samples
-    C80= 10.0 * np.log10((np.sum(ETC[:t]) / np.sum(ETC[t:])))
+    C80= 10.0 * np.log10((np.sum(sch[:t]) / np.sum(sch[t:])))
     
     return C80 
 
@@ -265,7 +270,7 @@ def limits_iec_61260(index, b, fr=1000):
 
 
 
-def lundeby(ETC, maf_window, fs):
+def lundeby(ETC, maf_window, band, fs):
     dB_to_noise = 7 # dB above noise for linear regression
     interval_density = 5 #number of time intervals per each 10 dB of dynamic range
     idx_last_10percent = -int(ETC.size/10) #start index of last 10% of signal
@@ -277,8 +282,7 @@ def lundeby(ETC, maf_window, fs):
     ETC_avg_dB = 10 * np.log10(ETC_averaged)
     
     # 2) Estimate noise level with last 10% of the signal
-    noise_estimate = 10 * np.log10( np.mean(ETC_averaged[idx_last_10percent:]) )
-
+    noise_estimate = 10 * np.log10( np.mean(ETC_averaged[idx_last_10percent:]) )        
     
     # 3) Estimate preliminar slope
     idx_stop = np.where(ETC_avg_dB >= noise_estimate + dB_to_noise)[0][-1]
@@ -294,6 +298,10 @@ def lundeby(ETC, maf_window, fs):
     dyn_range = lin_reg.intercept-noise_estimate
     interval_num = np.intc(interval_density * dyn_range / 10)
     new_window = np.intc(idx_stop / interval_num)
+    #Exception for low dynamic range
+    if dyn_range < dB_to_noise + late_dyn_range:
+        print(band, "Hz band: Dynamic Range too low!")
+        return ETC, ETC.size
     
     # 6) Moving average filter with new window
     ETC_averaged = moving_average(ETC, new_window)
@@ -329,8 +337,10 @@ def lundeby(ETC, maf_window, fs):
 
         # 9) Find new crosspoint
         crossing_point = np.argmin(np.abs(line - noise_estimate))
+        #Exception for too many loops
         if counter > 30:
-            print('Could not achieve convergence. Abort!')
+            print(band, 'Hz band: Could not achieve convergence. Abort!')
+            crossing_point = ETC.size
             break
         
         counter += 1
